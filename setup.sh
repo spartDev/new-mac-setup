8d9460added6ca88e64295eb877bcaf7514e9ae2
Thomas Roux <roux.t@sfeir.com>
2022-06-26 18:14:05 +0200

    fix typo on setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -euoE pipefail
 
-declare -r GITHUB_REPOSITORY="spartDev/dotfiles"
+declare -r GITHUB_REPOSITORY="spartDev/new-mac-setup"
 
 declare -r DOTFILES_ORIGIN="git@github.com:$GITHUB_REPOSITORY.git"
 declare -r DOTFILES_TARBALL_URL="https://github.com/$GITHUB_REPOSITORY/tarball/main"
